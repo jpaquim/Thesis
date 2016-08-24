d56c1481ed76ce25aa6c1208241e3d4e8fa420d0
guidoAI <guido.de.croon@gmail.com>
2016-08-24 10:09:41 +0200

    Testing of model on possibly separate test set is working

--- test_model.m
+++ test_model.m
@@ -21,46 +21,67 @@ testDataset = 'HEIGHT-test';
 % load the trained model to make predictions on the test set:
 load([trainDataset '-model-train.mat']);
 
+GRAPHICS = true;
+WAIT = false;
+
 % structure that contains the configuration of the image, patches, textons
 cfg = defaultConfig(trainDataset);
-cfg.stepSize = 1; % necessary for showing images
 
+% get all file names:
 [imgFiles,depthFiles,fileNumbers] = dataFilePaths(testDataset,'all', true);
 
 % precompute auxiliary variables needed afterwards, and update cfg
 % we now do it here to get the depths at the right positions
 cfg = computeAuxVars(cfg);
 
-h = figure('Name', 'Predicted depths', 'NumberTitle', 'off');
-set(gcf, 'Color', [1 1 1]);
 meanDepths= zeros(length(depthFiles), 1);
 medianDepths= zeros(length(depthFiles), 1);
 Depths = zeros(length(depthFiles), 1);
 for i = 1:length(depthFiles)
+    
+    fprintf('Image %d / %d\n', i, length(depthFiles));
+    
     % get depths and features:
-    depths = generateDepthsData(depthFiles,cfg);
+    depths = generateDepthsData(depthFiles(i),cfg);
     Depths(i) = mean(depths);
-    features = generateFeaturesData(imgFiles,cfg);
+    features = generateFeaturesData(imgFiles(i),cfg);
     % normalize the training features to the [0 1] range
-    [features,offset,scale] = normalizeFeatures(features);
+    features = normalizeFeatures(features,offset,scale);
     % add a bias:
     features = [features ones(size(features,1),1)];
     % predict depths:
     predictions = predictCLS(features, model);
     medianDepths(i) = median(predictions);
     meanDepths(i) = mean(predictions);
-    % get the performance metrics:
-    [logError,relativeAbsoluteError,relativeSquareError,...
-    rmsLinearError,rmsLogError,scaleInvariantError] = ...
-    performanceMetrics(predDepths,depths,dataset);
-    % show the image and depth image:
-    Im = imread(imFiles{i}); 
-    figure(h);
-    subplot(1,2,1); imshow(Im); title('Image');
-    subplot(1,2,2); 
-    DIm = reshape(predDepths, [cfg.nRows, cfg.nCols]);
-    imagesc(DIm); title('Depths');
+    
+    fprintf('Depth = %f, median pred = %f, mean pred = %f\n',  Depths(i),  medianDepths(i),  meanDepths(i));
+    
+    if(GRAPHICS && cfg.stepSize == 1)
+        % show the image and depth image:
+        Im = imread(imgFiles{i});
+        if(exist('h', 'var'))
+            close(h);
+        end
+        h = figure();
+        subplot(1,2,1); imshow(Im); title('Image');
+        subplot(1,2,2);
+        DIm = reshape(predictions, [cfg.nRows, cfg.nCols]);
+        imagesc(DIm); caxis([cfg.minRange, cfg.maxRange]);
+        colorbar
+        title(['Depths - avg = ' num2str(meanDepths(i)) ' GT = ' num2str(Depths(i))]);
+        if(WAIT)
+            waitforbuttonpress();
+        else
+            pause(0.1);
+        end
+    end
+    
 end
+
+% get the performance metrics:
+    [logError,relativeAbsoluteError,relativeSquareError,...
+        rmsLinearError,rmsLogError,scaleInvariantError] = ...
+        performanceMetrics(meanDepths,Depths,dataset);
 
 % plot depths per image:
 h2 = figure();
@@ -69,3 +90,4 @@ hold on;
 plot(1:length(depthFiles), meanDepths);
 plot(1:length(depthFiles), medianDepths);
 legend('GT depths', 'Mean predictions', 'Median predictions');
+
